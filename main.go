bfc191cb9fb5c54afce12f949f131ed8f1d231f6
Zili <ziliwesley@live.com>
2018-09-23 20:02:40 +0800

    Add an example of **hex dumping** given data

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 import caeser "github.com/ziliwesley/serious-cryptography/classical/caesar"
+import "encoding/hex"
 
 func main() {
     plainText := "Hello from Caesar!"
@@ -13,4 +14,12 @@ func main() {
     decrypted := caeser.DecryptClassic(cipher)
 
     fmt.Printf("Decrypted: %s\n", decrypted)
+
+    bytes := make([]byte, len(plainText))
+    copy(bytes[:], plainText)
+
+    fmt.Printf("%s\n", hex.Dump(bytes))
+    // use the "space" flag in the format, putting a space between the % and 
+    // the x.
+    fmt.Printf("% x", plainText)
 }
