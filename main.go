920c00f713d042cf6d8b1dae47a0c748146cc919
Zili <ziliwesley@live.com>
2018-09-23 20:35:38 +0800

    Add an example of generating random bytes

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 import caeser "github.com/ziliwesley/serious-cryptography/classical/caesar"
 import "encoding/hex"
+import "math/rand"
+import "time"
 
 func main() {
     plainText := "Hello from Caesar!"
@@ -21,5 +23,22 @@ func main() {
     fmt.Printf("%s\n", hex.Dump(bytes))
     // use the "space" flag in the format, putting a space between the % and 
     // the x.
-    fmt.Printf("% x", plainText)
+    fmt.Printf("% x\n", plainText)
+
+    // Generating a random, fixed-length byte array in Go
+    randBytes := make([]byte, len(plainText))
+    // Initialize random seed to make sure the result the result will change 
+    // every time `rand.Read()` was called
+    rand.Seed(time.Now().UnixNano())
+    rand.Read(randBytes)
+    fmt.Printf("Random Bytes: %x\n", randBytes)
+    encoded := hex.EncodeToString(randBytes)
+    fmt.Printf("Encoded: %s\n", encoded)
+
+    // Decode hex string to bytes
+    decoded, err := hex.DecodeString("41e8d461c6c11ceee9b40de6d7a9334279ec")
+    if (err != nil) {
+        fmt.Println("error decoding")
+    }
+    fmt.Printf("%x\n", decoded)
 }
